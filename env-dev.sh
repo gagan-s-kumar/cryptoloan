3ac7e650f494f2928a10820d425a16a0fcabedcb
naomimachado21 <naomimachado21@gmail.com>
2018-04-16 20:58:45 -0400

    working

--- env-dev.sh
+++ env-dev.sh
@@ -2,7 +2,7 @@
 
 export COINBASE_CLIENT_ID="80d081fbb195a4edc41aedc8f18bfd4454b0fe8eddd176741d46ee01b7f2c6f6"
 export COINBASE_CLIENT_SECRET="2e3c50382ed9885d1f7b9c421cce1ab1218a6ed2ca0dd4cd3fd83e4ff2b7827d"
-export COINBASE_REDIRECT_URI="http://demo.purneshdixit.stream/auth/coinbase/callback"
+export COINBASE_REDIRECT_URI="http://localhost:4000/auth/coinbase/callback"
 
 export SMTP_USERNAME="bitcoinalertsend"
 export SMTP_PASSWORD="qwerty456**"
