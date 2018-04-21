eaca0f66a8fc1dd3e220fdcfeb3d342f9775f230
naomimachado21 <naomimachado21@gmail.com>
2018-04-21 19:38:16 -0400

    css done

--- lib/cryptoloan/email.ex
+++ lib/cryptoloan/email.ex
@@ -2,14 +2,12 @@ defmodule Cryptoloan.Email do
   use Bamboo.Phoenix, view: Cryptoloan.EmailView
 
   def bitcoin_text_email(to_email, value) do
-    IO.puts "sending mails"
-    IO.inspect to_email
     value = Float.to_string(value)
-    send_string = "Bitcoin value " <> value
+    send_string = "Current Bitcoin value " <> value
     new_email
     |> to(to_email)
     |> from("bitcoinalertsend@gmail.com")
-    |> subject("Asli value!")
+    |> subject("BitCoin Price Drop!")
     |> text_body(send_string)
   end
 
@@ -17,11 +15,11 @@ defmodule Cryptoloan.Email do
     IO.puts "sending mails"
     IO.inspect to_email
     value = Float.to_string(value)
-    send_string = "Litecoin value " <> value
+    send_string = "Current Litecoin value " <> value
     new_email
     |> to(to_email)
     |> from("bitcoinalertsend@gmail.com")
-    |> subject("Asli value!")
+    |> subject("LiteCoin Price Drop!")
     |> text_body(send_string)
   end
 
@@ -29,11 +27,11 @@ defmodule Cryptoloan.Email do
     IO.puts "sending mails"
     IO.inspect to_email
     value = Float.to_string(value)
-    send_string = "Ethereum value " <> value
+    send_string = "Current Ethereum value " <> value
     new_email
     |> to(to_email)
     |> from("bitcoinalertsend@gmail.com")
-    |> subject("Asli value!")
+    |> subject("Ethereum Price Drop!")
     |> text_body(send_string)
   end
 
